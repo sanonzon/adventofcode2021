b01f518d28bdb35dd148da5d68bda59f39c5e0ad
sanonzon <pennsater@gmail.com>
2021-12-02 20:14:56 +0100

    skoja. dag 1

--- src/main/java/com/sudd/adventofcode/day1/Day1.java
+++ src/main/java/com/sudd/adventofcode/day1/Day1.java
@@ -1,5 +1,7 @@
 package com.sudd.adventofcode.day1;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -8,7 +10,7 @@ import java.util.List;
  * 
  */
 public class Day1 {
-    public int calc(List<Integer> numbers) {
+    public int part_one(List<Integer> numbers) {
         int higherCount = 0;
 
         Integer recent = null;
@@ -20,5 +22,46 @@ public class Day1 {
         }
 
         return higherCount;
+    }
+
+    public int part_two(List<Integer> numbers) {
+        int increments = 0;
+        Integer lastSum = null;
+        List<Integer> collection_1 = new ArrayList<>();
+        List<Integer> collection_2 = new ArrayList<>();
+        List<Integer> collection_3 = new ArrayList<>();
+
+        LinkedList<List<Integer>> collections = new LinkedList<>();
+        collections.add(collection_1);
+        collections.add(collection_2);
+        collections.add(collection_3);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            Integer number = numbers.get(i);
+
+            if (i == 0) { // First list
+                collections.get(0).add(number);
+            } else if (i == 1) { // First and Second list
+                collections.get(0).add(number);
+                collections.get(1).add(number);
+            } else { // All lists
+                collections.get(0).add(number);
+                collections.get(1).add(number);
+                collections.get(2).add(number);
+            }
+
+            for (List<Integer> list : collections) {
+                if (list.size() == 3) {
+                    int sum = list.stream().mapToInt(p -> p.intValue()).sum();
+                    if (lastSum != null && sum > lastSum) {
+                        increments++;
+                    }
+                    lastSum = sum;
+                    list.clear();
+                }
+            }
+        }
+
+        return increments;
     }
 }
--- src/test/java/com/sudd/adventofcode/Day1Test.java
+++ src/test/java/com/sudd/adventofcode/Day1Test.java
@@ -18,7 +18,7 @@ public class Day1Test {
         Day1 depth = new Day1();
 
         List<Integer> ints = Arrays.asList(1, 3, 5, 2, 2, 2, 2, 2, 2);
-        assertEquals(2, depth.calc(ints));
+        assertEquals(2, depth.part_one(ints));
     }
 
     @Test
@@ -26,7 +26,7 @@ public class Day1Test {
         Day1 depth = new Day1();
 
         List<Integer> ints = Arrays.asList(1, 2, 3, 5, 2, 2, 2, 2, 2, 2);
-        assertEquals(3, depth.calc(ints));
+        assertEquals(3, depth.part_one(ints));
     }
 
     @Test
@@ -34,13 +34,42 @@ public class Day1Test {
         Day1 depth = new Day1();
 
         List<Integer> ints = Arrays.asList(199, 200, 208, 210, 200, 207, 240, 269, 260, 263);
-        assertEquals(7, depth.calc(ints));
+        assertEquals(7, depth.part_one(ints));
     }
 
     @Test
     public void doot_do_calc() throws IOException {
         Day1 depth = new Day1();
-        System.out.println(depth.calc(InputHelper.readIntsFromFile("day1.txt")));
+        System.out.println(depth.part_one(InputHelper.readIntsFromFile("day1.txt")));
     }
 
+    @Test
+    public void doot_should_be_6() {
+        Day1 depth = new Day1();
+
+        List<Integer> ints = Arrays.asList(
+                1, // A
+                2, // A B
+                3, // A B C
+                4, // B C D
+                5, // C D E
+                6, // D E F
+                7, // E F G
+                8, //
+                9//
+
+        );
+        assertEquals(6, depth.part_two(ints));
+    }
+
+    @Test
+    public void doot_input_challenge() throws IOException {
+        Day1 depth = new Day1();
+
+        List<Integer> ints = InputHelper.readIntsFromFile("day1.txt");
+        System.out.println(depth.part_two(ints));
+
+        // Result reported to AoC
+        assertEquals(1257, depth.part_two(ints));
+    }
 }
